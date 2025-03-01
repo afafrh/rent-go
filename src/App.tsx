8fd73a95adb481a42c3188966087f050304ff729
afaf rhif <afaf.rhif@doctolib.com>
2025-03-01 23:45:09 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,8 @@ import "./App.css";
 import { Home } from "./pages/Home";
 import Booking from "./pages/Booking";
 import "./i18n";
-import { useTranslation } from "react-i18next";
 
 function App() {
-  const { t, i18n } = useTranslation();
   return (
     <Routes>
       <Route path="/" element={<Home />} />
--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import BookingHero from "../Components/BookingHero";
 import BookingForm from "../Components/BookingForm";
-import LanguageSelector from "../Components/LanguageSelector";
 
 const Booking: React.FC = () => {
   return (
